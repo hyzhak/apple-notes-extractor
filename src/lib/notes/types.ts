6ce3da969e2f7743a2488ccb1367c6ba156fccd0
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2025-12-05 00:57:37 -0800

    chore: prune unused notes bridge code

--- src/lib/notes/types.ts
+++ src/lib/notes/types.ts
@@ -1,11 +1,3 @@
-export interface NoteHeader {
-  id: string;
-  name: string | null;
-  folderPath?: string | null;
-  createdAtUtc?: string | null;
-  modifiedAtUtc?: string | null;
-}
-
 export interface NotesReaderOptions {
   runJxa?: (fn: (...args: any[]) => any, ...args: any[]) => Promise<any>;
   limit?: number;
@@ -19,25 +11,6 @@ export interface RawNoteResult {
   createdAtUtc: unknown;
   modifiedAtUtc: unknown;
 }
-
-export type JXANote = {
-  id?: () => unknown;
-  uuid?: () => unknown;
-  name?: () => unknown;
-  body?: () => unknown;
-  creationDate?: () => unknown;
-  modificationDate?: () => unknown;
-};
-
-export type JXAFolder = {
-  name?: () => unknown;
-  notes?: () => unknown;
-  folders?: () => unknown;
-};
-
-export type JXAAccount = {
-  folders?: () => unknown;
-};
 
 export type NotesApp = {
   includeStandardAdditions: boolean;
